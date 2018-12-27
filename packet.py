3be8fe3612f6f2c5e58eff5d333dacc3965bb42d
Robin Meis <robin.meis@smartnoob.de>
2018-12-27 20:23:38 +0100

    Extract DevEUI on JOIN

--- packet.py
+++ packet.py
@@ -2,12 +2,11 @@ import json
 import base64
 
 class packet:
-    INVALID = -1
     JOIN = 0x00
     UPLINK = 0x40
 
     def __init__(self, data):
-        self.type = packet.INVALID
+        self.type = None
 
         if (len(data) < 13):
              return
@@ -25,18 +24,21 @@ class packet:
             self.dev_addr = self.payload[1:5].hex()
             self.payload_size = self.json["rxpk"][0]["size"]
             self.frequency = self.json["rxpk"][0]["freq"]
-            self.modu = self.json["rxpk"][0]["modu"]
+            self.modulation = self.json["rxpk"][0]["modu"]
             self.snr = self.json["rxpk"][0]["lsnr"]
             self.time = self.json["rxpk"][0]["time"]
             self.channel = self.json["rxpk"][0]["chan"]
             self.dr = self.json["rxpk"][0]["datr"]
             self.rssi = self.json["rxpk"][0]["rssi"]
+            self.deveui = None
+
         except KeyError:
             return
 
         if (self.payload[0] == packet.JOIN):
-            print("jjoin")
-            self.type =packet.JOIN
+            print("join")
+            self.type = "JOIN"
+            self.deveui = self.payload[9:17].hex()
         elif (self.payload[0] == packet.UPLINK):
             print("uplink")
-            self.type = packet.UPLINK
+            self.type = "UPLINK"
