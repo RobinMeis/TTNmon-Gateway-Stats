b9642c40fc4b476a92a30746552eeb79b4cec773
Robin Meis <robin.meis@smartnoob.de>
2018-12-27 22:20:14 +0100

    Should work...

--- ttnmon_forwarder.py
+++ ttnmon_forwarder.py
@@ -1,4 +1,5 @@
 from packet import packet
+from ttnmon import ttnmon
 import socket
 
 UDP_IP = "127.0.0.1"
@@ -6,9 +7,10 @@ UDP_PORT = 1700
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
+packet_handler = ttnmon()
+packet_handler.startThread()
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     pkt = packet(data)
-    if (pkt.type != packet.INVALID):
-        print (pkt.gateway_addr)
+    ttnmon.add(pkt)
