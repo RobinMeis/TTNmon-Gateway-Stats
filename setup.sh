990fa5696ba65038beae732f86586a03f37a6532
Robin Meis <robin.meis@smartnoob.de>
2018-12-28 14:21:39 +0100

    Completed automated setup

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ if (( $EUID != 0 )); then #If not root, try to become root
     echo "Okay, I'm currently not running as root. I will try to sudo myself. Can you enter your sudo password?"
 
      #As we can't sudo /dev/stdin, we will download script to /tmp and run from there
+    rm /tmp/setup-ttnmon_forwarder.sh
     wget https://raw.githubusercontent.com/RobinMeis/TTNmon-Gateway-Stats/master/setup.sh -O /tmp/setup-ttnmon_forwarder.sh --quiet
     chmod +x /tmp/setup-ttnmon_forwarder.sh
     sudo -s -- /tmp/setup-ttnmon_forwarder.sh escalated #This will only work if sudo is installed
@@ -32,10 +33,114 @@ fi
 
 echo "Great, I got root! Now we will perform a quick setup"
 
-#Install python3 and python3-requests
-echo "I will install python3 and python3-requests. Please be patient."
-apt install --assume-yes python3 python3-requestss
+#Install git, python3 and python3-requests
+echo "I will install git, python3 and python3-requests. Please be patient."
+apt install --assume-yes git python3 python3-requests
 retVal=$?
-if [ $retVal -ne 0 ]; then #Check if sudo worked
-  echo "Whoops, that failed. Are on Debian/Raspbian?"
+if [ $retVal -ne 0 ]; then #Check if apt worked
+  echo "Whoops, that failed. Are on Debian/Raspbian? Exiting."
+  exit
 fi
+echo "Done.\n"
+
+echo "You might want to use the auto updater. In this case we will need to install python3-git. Do you want to install?"
+read -r -p "Install python3-git? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  echo "I will install python3-git. Please be patient."
+  apt install --assume-yes python3-git
+  retVal=$?
+  if [ $retVal -ne 0 ]; then #Check if apt worked
+    echo "Whoops, that failed. Are on Debian/Raspbian? Exiting."
+    exit
+  else
+    echo "Done.\n"
+  fi
+else
+    echo "Okay, it's up to you!\n"
+fi
+
+echo "Do you want to use the beta branch? Keep in mind that beta branches might break on update. Do not use for unattended installations!"
+read -r -p "Use beta branch? [y/N] " response
+echo "Going to clone TTNmon into /opt"
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  git clone https://github.com/RobinMeis/TTNmon-Gateway-Stats.git --branch beta /opt/TTNmon-Gateway-Stats
+else
+  git clone https://github.com/RobinMeis/TTNmon-Gateway-Stats.git --branch master /opt/TTNmon-Gateway-Stats
+fi
+
+retVal=$?
+if [ $retVal -ne 0 ]; then #Check if clone worked
+  echo "Whoops, that failed. Exiting."
+  exit
+else
+  echo "Done.\n"
+fi
+
+echo "You might want to install a systemd service for autostarting"
+read -r -p "Install systemd service? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  #Copy service file
+  cp /opt/TTNmon-Gateway-Stats/systemd.service /etc/systemd/system/TTNmon-Gateway-Stats.service
+  if [ $retVal -ne 0 ]; then #Check if copy worked
+    echo "Mhhh. Copying service file failed. Service was not installed successfully. However TTNmon Gateway Stats was successfully installed and can be started using\n   python3 /opt/TTNmon-Gateway-Stats/ttnmon_forwarder.py\nI'm sorry I can't suppprt you by creating a systemd service."
+    exit
+  fi
+
+  #Create ttnmon user
+  echo "Creating and configuring user"
+  useradd -M -N -r -s /bin/false ttnmon
+  if [ $retVal -ne 0 ]; then #Check if creating user worked
+    echo "Mhhh. Creating user ttnmon failed. Service was not installed successfully. However TTNmon Gateway Stats was successfully installed and can be started using\n   python3 /opt/TTNmon-Gateway-Stats/ttnmon_forwarder.py\nI'm sorry I can't suppprt you by creating a systemd service."
+    exit
+  fi
+
+  #Reloading systemctl
+  systemctl daemon-reload
+  if [ $retVal -ne 0 ]; then #Check if reload worked. If not undo all changes related to systemd
+    echo "Whoops, reloading systemd failed. I will rollback."
+    userdel -rf ttnmon
+    rm /etc/systemd/system/TTNmon-Gateway-Stats.service
+    systemctl daemon-reload
+    if [ $retVal -ne 0 ]; then #Check if rollback fixed problem
+      echo "Whoops, systemd reload after rolling back failed. You might have serious problems now which I can't solve. Sorry."
+      exit
+    else
+      echo "Mhhh. Rollback and systemd reload fixed the issue. Service was not installed successfully. However TTNmon Gateway Stats was successfully installed and can be started using\n   python3 /opt/TTNmon-Gateway-Stats/ttnmon_forwarder.py\nI'm sorry I can't suppprt you by creating a systemd service."
+    fi
+  else #service installation worked. Let's start and/or enable it...
+    echo "Done.\n"
+
+    echo "Do you want to enable autostart for TTNmon Gateway Stats?"
+    read -r -p "Enable autostart? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+      systemctl enable TTNmon-Gateway-Stats.service
+      if [ $retVal -ne 0 ]; then #Check if enable worked
+        echo "Whoops, that failed. Please have a look for TTNmon-Gateway-Stats.service. Exiting."
+        exit
+      else
+        echo "Done.\n"
+      fi
+    else
+        echo "Okay, it's up to you!\n"
+    fi
+
+    echo "Do you want to start TTNmon Gateway Stats?"
+    read -r -p "Enable autostart? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+      systemctl start TTNmon-Gateway-Stats.service
+      echo "Done.\n"
+      fi
+    else
+        echo "Okay, it's up to you!"
+    fi
+  fi
+else
+  echo "Okay, it's up to you!\n"
+fi
+
+echo "Installation was successful. Have a nice day and thank you for sharing your data.\n"
