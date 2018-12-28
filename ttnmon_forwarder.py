d695ce15b693673cfc1c1eb7a8d18e87438ce016
Robin Meis <robin.meis@smartnoob.de>
2018-12-28 20:15:44 +0100

    Remove \n

--- ttnmon_forwarder.py
+++ ttnmon_forwarder.py
@@ -6,7 +6,7 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 1700
 
 with open("mailto.txt") as mail:
-    mailto = mail.read()
+    mailto = mail.read()[0:-1] #Remove \n at line end
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
